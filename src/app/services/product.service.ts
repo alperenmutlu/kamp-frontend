7a3d9976a9d80451a6911920bf49f2da0eacba35
Alperen Mutlu <chetto_17@hotmail.com>
2021-03-18 00:06:17 +0300

    Sepet ekle-sil eklendi

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { ProductResponseModel } from 'src/app/models/productResponseModel';
 import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  apiUrl = 'https://localhost:44336/api/products/getall';
+  apiUrl = 'https://localhost:44336/api/';
 
   constructor(private httpClient: HttpClient) { }
 
-  getProducts():Observable<ProductResponseModel> {
-    return this.httpClient
-      .get<ProductResponseModel>(this.apiUrl); //gelen datayı ProductResponse modeline map edicek.    
+  getProducts():Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getall"
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
+    let newPath = this.apiUrl + "products/getbycategory?categoryId="+categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
 }
